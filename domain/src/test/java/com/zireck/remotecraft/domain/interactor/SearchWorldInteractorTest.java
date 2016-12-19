46c21ff119b81508d81387c697767d723985aedb
Zireck <andreshj87@gmail.com>
2016-12-19 11:36:53 +0100

    Added a few more tests

--- domain/src/test/java/com/zireck/remotecraft/domain/interactor/SearchWorldInteractorTest.java
+++ domain/src/test/java/com/zireck/remotecraft/domain/interactor/SearchWorldInteractorTest.java
@@ -1,16 +1,21 @@
 package com.zireck.remotecraft.domain.interactor;
 
+import com.zireck.remotecraft.domain.World;
 import com.zireck.remotecraft.domain.executor.PostExecutionThread;
 import com.zireck.remotecraft.domain.executor.ThreadExecutor;
 import com.zireck.remotecraft.domain.provider.NetworkProvider;
+import io.reactivex.Maybe;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
 
 public class SearchWorldInteractorTest {
 
@@ -33,5 +38,41 @@ public class SearchWorldInteractorTest {
     verifyNoMoreInteractions(mockNetworkProvider);
     verifyZeroInteractions(mockThreadExecutor);
     verifyZeroInteractions(mockPostExecutionThread);
+  }
+
+  @Test public void shoulNotReturnInvalidReactiveStreamWhenValidWorldFound() throws Exception {
+    when(mockNetworkProvider.searchWorld()).thenReturn(getValidWorldReactiveStream());
+
+    Maybe reactiveStream = searchWorldInteractor.buildReactiveStream();
+
+    assertNotNull(reactiveStream);
+  }
+
+  @Test public void shouldNotReturnInvalidReactiveStreamWhenNoWorldFound() throws Exception {
+    when(mockNetworkProvider.searchWorld()).thenReturn(Maybe.empty());
+
+    Maybe reactiveStream = searchWorldInteractor.buildReactiveStream();
+
+    assertNotNull(reactiveStream);
+  }
+
+  @Test public void shouldReturnEmptyReactiveStreamWhenNoWorldFound() throws Exception {
+    when(mockNetworkProvider.searchWorld()).thenReturn(Maybe.empty());
+
+    Maybe reactiveStream = searchWorldInteractor.buildReactiveStream();
+
+    assertEquals(getEmptyReactiveStream(), reactiveStream);
+  }
+
+  private Maybe<World> getValidWorldReactiveStream() {
+    return Maybe.just(getFakeWorld());
+  }
+
+  private World getFakeWorld() {
+    return new World.Builder().build();
+  }
+
+  private Maybe<World> getEmptyReactiveStream() {
+    return Maybe.empty();
   }
 }
