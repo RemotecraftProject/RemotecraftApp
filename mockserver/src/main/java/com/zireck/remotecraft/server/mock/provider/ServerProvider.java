94c15fecff24e3a325786ef0c0660b082e6802f2
Zireck <andreshj87@gmail.com>
2017-04-06 09:02:06 +0200

    Removed MockServer module

--- mockserver/src/main/java/com/zireck/remotecraft/server/mock/provider/ServerProvider.java
+++
@@ -1,7 +0,0 @@
-package com.zireck.remotecraft.server.mock.provider;
-
-import com.zireck.remotecraft.server.mock.protocol.messages.ServerMessage;
-
-public interface ServerProvider {
-  ServerMessage getServerData();
-}
